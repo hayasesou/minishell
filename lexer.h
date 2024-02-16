0b3a00f56eee79c12443feb2ca7a8acd2df4de5f
hakobaya-log <hakobaya.0415@gmail.com>
2024-02-16 20:14:22 +0900

    enum変更

--- lexer.h
+++ lexer.h
@@ -6,17 +6,17 @@
 /*   By: hakobaya <hakobaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 16:50:29 by hakobaya          #+#    #+#             */
-/*   Updated: 2024/02/16 19:20:55 by hakobaya         ###   ########.fr       */
+/*   Updated: 2024/02/16 20:13:43 by hakobaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <ctype.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <ctype.h>
 
 // readlineから読まれた入力値をtokenに格納するための構造体
 
@@ -32,11 +32,37 @@ typedef struct s_token {
 
 //enum列挙型
 typedef enum e_token_type {
-	PIPE,
-	STRING,
-	SEMICORON
+	E_ASTERISK, //*
+	E_AT, //@
+	E_SHARP, //#
+	E_HATENA, //?
+	E_HYPHEN, //-
+	E_DOLLAR, //$
+	E_EXCLAMATION, //!
+	E_ZERO, //($0) シェルまたはシェル スクリプトの名前に展開される
+	E_COLON,
+	E_PERIOD,
+	E_BREAK,
+	E_CD,
+	E_CONTINUE,
+	E_EVAL,
+	E_EXEC,
+	E_EXIT,
+	E_EXPORT,
+	E_GETOPTS,
+	E_HASH,
+	E_PWD,
+	E_READONLY,
+	E_RETURN,
+	E_TEST,
+	E_TIMES,
+	E_TRAP,
+	E_UMASK,
+	E_UNSET,
+	E_SEMICOLON,
+	E_PIPE,
+	E_STRING,
 }			t_token_type;
-//今後足していく
-
+//今後足していく、GNU bash manual 4.1ビルトイン関数の部分参照
 
 #endif
