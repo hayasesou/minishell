3dbeeab24eb6f8c034419ca8be25adc7faf8aedd
hayasesou <f.h.sassou@icloud.com>
2024-04-18 18:47:03 +0900

    piep

--- pipe/test.c
+++ pipe/test.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+#define WRITE 1
+#define READ 0
 
 typedef struct s_pipex
 {
@@ -19,49 +21,40 @@ int main(int argc, char **argv, char **envp)
 {
     t_pipex pipex;
     pid_t pid1;
+    // pid_t pid2;
 
     (void)argc;
     (void)argv;
     (void)envp;
-    char *buf;
-    int count ;
 
-    buf = (char*)malloc(sizeof(char) * bufsize);
 
     if (pipe(pipex.pipefd) == -1)
         exit(1);
     
-    char *file_path = "/usr/bin/ls";
-    char *args[] = {"ls", "-l", NULL};
+    char *file_path1 = "/usr/bin/ls";
+    char *args1[] = {"ls", "-l", NULL};
+    char *file_path2 = "/usr/bin/grep";
+    char *args2[] = {"grep", "out", NULL};
 
     pid1 = fork();
     if(pid1 == 0)
     {
-        printf("child process\n");
-        dup2(pipex.pipefd[1], STDOUT_FILENO);
-        close(pipex.pipefd[1]);
-        close(pipex.pipefd[0]);
-        execve(file_path, args, envp);
+        dup2(pipex.pipefd[WRITE], STDOUT_FILENO);
+        close(pipex.pipefd[READ]);
+        execve(file_path1, args1, envp);
     
         printf("execv failed\n");
     }
-    else
-    {
-        waitpid(pid1, NULL, 0);
-        while(1)
-        {
-            // write(1, "parent process\n", 16);
-            count = read(pipex.pipefd[0], buf, bufsize);
-            if (count < bufsize)
-            {
-                write(STDOUT_FILENO, buf, count);
-                break;
-            }
-
-            write(STDOUT_FILENO, buf, bufsize);
-        }
-        printf("parent process\n");
-    }
+    // pid2 = fork();
+    // if(pid2 == 0)
+    // {
+        close(pipex.pipefd[WRITE]);
+        dup2(pipex.pipefd[READ], STDIN_FILENO);
+        execve(file_path2, args2, envp);
+        
+    // }
+    // waitpid(pid1, NULL, 0);
+    // write(1, "parent process\n", 16);
 
     return 0;
 }
