99606c280357a365d47e4216bcb8a81503eeeca1
hakobaya-log <hakobaya.0415@gmail.com>
2024-05-23 15:54:17 +0900

    構造体中身消えてたから書き直した

--- include/struct.h
+++ include/struct.h
@@ -6,7 +6,7 @@
 /*   By: hakobaya <hakobaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 16:50:29 by hakobaya          #+#    #+#             */
-/*   Updated: 2024/05/23 15:22:48 by hakobaya         ###   ########.fr       */
+/*   Updated: 2024/05/23 15:53:54 by hakobaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,54 +17,94 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <ctype.h>
+# include <stdbool.h>
+
+#define bool _Bool
+#define true 1
+#define false 0
 
 //enum列挙型
+//typedef enum e_token_type {
+//	CHAR,
+//	PIPE,
+//	QUOTE,
+//	CHAR_QUOTE,
+//	D_QUOTE,
+//	CHAR_D_QUOTE,
+//	GREATER,
+//	D_GREATER, // redirect
+//	LESSER,
+//	D_LESSER, // heredoc
+//	SEMICOLON,
+//	SPACE,
+//	TAB,
+//	IO_NUM,
+//	ESCAPE, // いる？
+//	//COMMAND
+//}			t_token_type;
+
 typedef enum e_token_type {
-	CHAR,
-	PIPE,
-	QUOTE,
-	CHAR_QUOTE,
-	D_QUOTE,
-	CHAR_D_QUOTE,
-	GREATER,
-	D_GREATER, // redirect
-	LESSER,
-	D_LESSER, // heredoc
-	SEMICOLON,
-	SPACE,
-	TAB,
-	IO_NUM,
-	ESCAPE, // いる？
-	//COMMAND
-}			t_token_type;
+	TK_REDIR_IN = 0,
+	TK_REDIR_OUT = 1,
+	TK_REDIR_APPEND = 2,
+	TK_REDIR_HEREDOC = 3,
+	TK_WORD,
+	TK_PIPE,
+	TK_EOF,
+}	t_token_type;
 
-typedef enum e_token_state {
-	IN_D_QUOTE,
-	IN_QUOTE,
-	GENERAL
-}			t_token_state;
+//typedef enum e_token_state {
+//	IN_D_QUOTE,
+//	IN_QUOTE,
+//	GENERAL
+//}			t_token_state;
 
-typedef struct s_token	t_token;
 
-struct	s_token {
-	char 			*data;
+typedef struct s_token {
+	char			*data;
 	t_token_type	type;
 	t_token			*next;
 	t_token			*prev;
-};
+}	t_token;
 
-typedef struct s_env t_env;
+typedef enum e_node_type {
+	ND_CMD,
+	ND_PIPE
+}	t_node_type;
 
-struct s_env {
+typedef struct s_redirect {
+	char			*file;
+	t_token_type	type;
+}	t_redirect;
+
+typedef struct s_list {
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+typedef struct s_ast {
+	t_node_type	type;
+	t_token		*argv;
+	t_list		*redir_lst;
+	t_ast		*left;
+	t_ast		*right;
+	t_token		*lst;
+}	t_ast;
+
+typedef struct s_env {
 	char	**line;
 	t_env	*next;
 	t_env	*prev;
-};
-
-typedef struct s_context t_context;
+}	t_env;
 
 struct s_context {
 	// 構造体の総まとめをこの構造体にまとめる
-};
+	t_token	*token;
+	t_ast	*ast;
+	t_env	*env;
+	int		status;
+	bool	sys_error;
+	bool	include_quote;
+}	t_context;
 
 #endif
