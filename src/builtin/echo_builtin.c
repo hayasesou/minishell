8270fa11a26b75ecd92b61300a44f1b17bf1b757
hakobaya <heily262626@gmail.com>
2024-10-02 17:19:05 +0900

    -nオプションの扱いを正しくした

--- src/builtin/echo_builtin.c
+++ src/builtin/echo_builtin.c
@@ -2,90 +2,40 @@
 
 static bool is_n_option(char *op)
 {
-    if (*op == '\0')
-        return (false);
-    if (ft_strncmp("-n", op, 2) == 0)
-    {
-        if (ft_strlen(op) <= 2)
-            return (true);
-        else
-            op += 2;
-    }
-    while (*op && *op == 'n')
-        op++;
-    if (*op)
-        return (false);
-    return (true);
-}
-
-static int count_command(char **cmd)
-{
-    int i;
-
-    i = 0;
-    while (cmd[i] != NULL)
-        i++;
-    return (i);
-}
-
-static void have_n_option(char **cmd)
-{
-    int i;
-    int cmd_size;
+    int    i;
 
     i = 1;
-    cmd_size = count_command(cmd);
-    while (i < cmd_size)
-    {
-        if (!is_n_option(cmd[i]))
-        {
-            ft_printf("%s", cmd[i]);
-            if (i < cmd_size - 1)
-                ft_printf(" ");
-        }
+    if (op[0] != '-')
+        return false;
+    while (op[i] == 'n')
         i++;
-    }
-}
-
-static void no_n_option(char **cmd)
-{
-    int i;
-    int cmd_size;
-
-    i = 1;
-    cmd_size = count_command(cmd);
-    while (i < cmd_size)
-    {
-        ft_printf("%s", cmd[i]);
-        if (i < cmd_size - 1)
-            ft_printf(" ");
-        i++;
-    }
-    ft_printf("\n");
+    if (op[i] != '\0')
+        return false;
+    return true;
 }
 
 void    echo_builtin(t_parser *parser, t_context *context)
 {
-    t_parser *tmp_parser;
+    int         i;
+    bool        n_option;
+    t_parser    *tmp_parser;
 
+    i = 1; // echoコマンドの次の引数から表示
+    n_option = false;
     tmp_parser = parser;
-    (void)context;
-    if (tmp_parser->cmd[1] && is_n_option(tmp_parser->cmd[1]))
+    while (tmp_parser->cmd[i] && is_n_option(tmp_parser->cmd[i]))
     {
-        while (tmp_parser)
-        {
-            have_n_option(tmp_parser->cmd);
-            tmp_parser = tmp_parser->next;
-        }
-        context->exit_status = 0;
+        n_option = true;
+        i++;
     }
-    else
+    while (tmp_parser->cmd[i])
     {
-        while (tmp_parser)
-        {
-            no_n_option(tmp_parser->cmd);
-            tmp_parser = tmp_parser->next;
-        }
-        context->exit_status = 0;
+        ft_printf("%s", tmp_parser->cmd[i]);
+        if (tmp_parser->cmd[i + 1])
+            ft_printf(" ");
+        i++;
     }
+    if (!n_option)
+        ft_printf("\n");
+    context->exit_status = 0;
 }
