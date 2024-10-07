ddf006f15c9e1f0f5eb621e007506e5b924e29ba
hakobaya <heily262626@gmail.com>
2024-10-07 13:54:32 +0900

    かんせい

--- src/signal/signal_handler.c
+++ src/signal/signal_handler.c
@@ -7,7 +7,7 @@ void signal_handler(int signum)
         g_signal = 1;
         ft_printf("\n");
         rl_on_new_line();
-//        rl_replace_line("", 0);
+        rl_replace_line("", 0);
         rl_redisplay();
     }
 }
@@ -26,7 +26,7 @@ void signal_parent_handler(int signum)
     {
         g_signal = signum;
         ft_printf("Quit: 3\n");
-//        rl_replace_line("", 0);
+        rl_replace_line("", 0);
         rl_on_new_line();
         rl_redisplay();
     }
