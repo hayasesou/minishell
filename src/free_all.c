d497a971886bf4f0fd57f7de3e996aebc82b791e
hakobaya <heily262626@gmail.com>
2024-10-09 17:25:59 +0900

    強制終了以外のfree大丈夫、強制時のダブルフリーを防ぐのを考え中

--- src/free_all.c
+++ src/free_all.c
@@ -8,7 +8,7 @@ void free_token(t_token *head)
     if (head == NULL)
         return;
     cur = head->next;
-    while (cur != head)
+    while(cur != head)
     {
         next = cur->next;
         free(cur->data);
@@ -17,7 +17,6 @@ void free_token(t_token *head)
     }
     free(head->data);
     free(head);
-
 }
 
 void free_file(t_file *head)
@@ -36,28 +35,28 @@ void free_file(t_file *head)
 
 void free_parser(t_parser *head)
 {
-	t_parser	*tmp;
-	t_parser	*delete;
-	int			i;
+    t_parser *tmp;
+    t_parser *delete;
+    int i;
 
-	if (head == NULL)
-		return ;
-	tmp = head;
-	while(tmp != NULL)
-	{
-		i = 0;
-		if (tmp->cmd != NULL)
-		{
-			while(tmp->cmd[i] != NULL)
-				free(tmp->cmd[i++]);
-			free(tmp->cmd);
-		}
-		if (tmp->file != NULL)
-			free_file(tmp->file);
-		delete = tmp;
-		tmp = tmp->next;
-		free(delete);
-	}
+    if (head == NULL)
+        return;
+    tmp = head;
+    while(tmp != NULL)
+    {
+        i = 0;
+        if (tmp->cmd != NULL)
+        {
+            while(tmp->cmd[i] != NULL)
+                free(tmp->cmd[i++]);
+            free(tmp->cmd);
+        }
+        if (tmp->file != NULL)
+            free_file(tmp->file);
+        delete = tmp;
+        tmp = tmp->next;
+        free(delete);
+    }
 }
 
 void free_env_node(t_env *node)
@@ -92,13 +91,22 @@ void free_env(t_env *head)
 void free_all(t_context *ctx)
 {
     if (ctx == NULL)
-        return ;
+        return;
     if (ctx->env_head != NULL)
+    {
         free_env(ctx->env_head);
+        ctx->env_head = NULL;
+    }
     if (ctx->token_head != NULL)
+    {
         free_token(ctx->token_head);
+        ctx->token_head = NULL;
+    }
     if (ctx->parser_head != NULL)
+    {
         free_parser(ctx->parser_head);
+        ctx->parser_head = NULL;
+    }
     free(ctx);
 }
 
