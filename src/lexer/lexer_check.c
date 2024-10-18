e5831c667791cece61c82b955265266a2bdb44e4
hakobaya-code <hakobaya.0415@gmail.com>
2024-10-19 04:17:55 +0900

    >>

--- src/lexer/lexer_check.c
+++ src/lexer/lexer_check.c
@@ -6,7 +6,7 @@
 /*   By: hakobaya <hakobaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 23:33:43 by hakobaya          #+#    #+#             */
-/*   Updated: 2024/10/18 23:36:58 by hakobaya         ###   ########.fr       */
+/*   Updated: 2024/10/19 04:17:31 by hakobaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,17 @@ bool	is_token_op(t_token *token)
 	return (false);
 }
 
+void	syntax_error_operation(char *msg, t_context *ctx)
+{
+	ft_putstr_fd("minishell: syntax error: near unexpected token ", 2);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+	free_token(&ctx->token_head);
+	ctx->token_head = NULL;
+	ctx->exit_status = SYNTAX_ERROR;
+	ctx->sys_error = true;
+}
+
 void	check_token_operation(t_context *ctx)
 {
 	t_token	*current;
@@ -32,12 +43,12 @@ void	check_token_operation(t_context *ctx)
 		{
 			if (is_token_op(current->next))
 			{
-				syntax_error_operation("\n", ctx);
+				syntax_error_operation("", ctx);
 				return ;
 			}
 			else if (current->next->type == TK_EOF)
 			{
-				syntax_error_operation("'newline'\n", ctx);
+				syntax_error_operation("'newline'", ctx);
 				return ;
 			}
 		}
